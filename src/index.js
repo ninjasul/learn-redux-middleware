4d6ab6c4f234590aa52260eb76725f14af4514e3
ninjasul@gmail.com <ninjasul@gmail.com>
2020-01-09 10:17:06 +0900

    redux-thunk 참조 부분 변경

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import {applyMiddleware, createStore} from "redux";
 import rootReducer from "./modules";
 import {logger} from "redux-logger";
 import {composeWithDevTools} from "redux-devtools-extension";
-import {ReduxThunk} from "redux-thunk";
+import thunk from "redux-thunk";
 
 const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+    composeWithDevTools(applyMiddleware(thunk, logger))
 );
 
 ReactDOM.render(
