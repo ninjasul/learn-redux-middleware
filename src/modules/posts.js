567f09e97876b3f290f81828450f27e17f2f973c
ninjasul@gmail.com <ninjasul@gmail.com>
2020-01-09 10:50:42 +0900

    리듀서 리팩토링

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -17,17 +17,20 @@ const initialState = {
     post: reducerUtils.initial()
 };
 
+const getPostsReducer = handleAsyncActions(GET_POSTS, 'posts');
+const getPostReducer = handleAsyncActions(GET_POST, 'post');
+
 export default function posts(state = initialState, action) {
     switch(action.type) {
         case GET_POSTS:
         case GET_POSTS_SUCCESS:
         case GET_POSTS_ERROR:
-            return handleAsyncActions(GET_POSTS, 'posts') (state, action);
+            return getPostsReducer(state, action);
 
         case GET_POST:
         case GET_POST_SUCCESS:
         case GET_POST_ERROR:
-            return handleAsyncActions(GET_POST, 'post') (state, action);
+            return getPostReducer(state, action);
 
         default:
             return state;
